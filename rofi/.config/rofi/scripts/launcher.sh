ac83193251a5c0057a42fdce6eddd1a5f41a9290
Georgy Zheyts <gzheyts@gmail.com>
2026-01-12 17:34:55 +0700

    [rofi] customize launchers

---
+++ rofi/.config/rofi/scripts/launcher.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+## Run
+rofi -show drun  -theme ~/.config/rofi/theme/launch.rasi
---
+++ rofi/.config/rofi/scripts/windows.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#
+rofi -show window  -theme ~/.config/rofi/theme/window.rasi
+

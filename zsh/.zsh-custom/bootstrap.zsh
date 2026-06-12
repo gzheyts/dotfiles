87a1d8cf04a3c4888061300a7c08df005499132f
Georgy Zheyts <gzheyts@gmail.com>
2026-06-12 15:07:55 +0700

    [zsh] use absolute path

--- zsh/.zsh-custom/bootstrap.zsh
+++ zsh/.zsh-custom/bootstrap.zsh
@@ -1,16 +1,18 @@
 #!/bin/zsh
 
-for alias_file in aliases/*(N); do
+local custom_dir="${0:a:h}"
+
+for alias_file in $custom_dir/aliases/*(N); do
     source $alias_file
 done
 unset alias_file
 
-for config_file in config/*(N); do
+for config_file in $custom_dir/config/*(N); do
     source $config_file
 done
 unset config_file
 
-for complation_file in completions/*(N); do
-    source $complation_file
+for completion_file in $custom_dir/completions/*(N); do
+    source $completion_file
 done
-unset complation_file
+unset completion_file
